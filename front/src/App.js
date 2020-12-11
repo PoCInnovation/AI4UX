1796aaea231df9a6e2a6b0a710cd198492d938b4
Alexandre G <alexandre.gouasmi@skillz.io>
2020-12-11 23:25:38 +0100

    *master: init front

---
+++ front/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p> Welcome to PoC submission for the hackathon AI4UX</p>
+    </div>
+  );
+}
+
+export default App;
