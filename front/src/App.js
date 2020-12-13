a2eaf912f40184ffc9ebd38de2572a30a1a7bf4a
Tom Chauveau <tom.chauveau@epitech.eu>
2020-12-13 13:52:49 +0100

    refactor(front): add responsive on homepage

--- front/src/App.js
+++ front/src/App.js
@@ -2,25 +2,36 @@ import './App.css';
 import {Button} from "@material-ui/core";
 
 function App() {
-  return (
-      <div>
-        <div className="App">
-            <p className="Title"> UX Analyzer</p>
-            <div>
-                <input type="text" placeholder="Enter an url"
-                    style={{ padding: "10px", borderRadius: "5px", width: "300px", height: "35px", outline: "none", border: 0, display: 'inline-block', marginRight: "20px", marginTop: "-10px"}}
-                />
-                <Button variant="contained" color="primary">
-                    Analyze
-                </Button>
+    return (
+        <div>
+            <div className="App">
+                <p className="Title"> UX Analyzer</p>
+                <div>
+                    <input type="text" placeholder="Enter an url"
+                           style={{
+                               padding: "10px",
+                               borderRadius: "5px",
+                               width: "300px",
+                               height: "35px",
+                               outline: "none",
+                               border: 0,
+                               display: 'inline-block',
+                               marginRight: "20px",
+                               marginTop: "-10px"
+                           }}
+                    />
+                    <Button variant="contained" color="primary">
+                        Analyze
+                    </Button>
+                </div>
+
+            </div>
+            <div className="Footer">
+                <p style={{display: "inline-block",}}>Made with love by</p>
+                <a style={{paddingLeft: "5px", color: "#DB4D55",}} href="http://poc-innovation.com/">PoC</a>
             </div>
         </div>
-          <div style={{color: "#FFF", textAlign: "center", position: "absolute", bottom: "0", left: "47%"}} >
-            <p style={{ display: "inline-block",}}>Made with love by</p>
-            <a style={{paddingLeft: "5px", color: "#DB4D55",}} href="http://poc-innovation.com/">PoC</a>
-          </div>
-      </div>
-  );
+    );
 }
 
 export default App;
