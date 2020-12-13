44aaf0516247cbd48959e263e47ea4035cb47484
Thytu <valentin.de-matos@epitech.eu>
2020-12-13 17:03:38 +0100

    *anal: fix responsiveness test

--- back/responsive.py
+++ back/responsive.py
@@ -3,7 +3,7 @@ import requests
 
 def responsiveness_tester(url):
     x = requests.post("https://searchconsole.googleapis.com/v1/urlTestingTools/mobileFriendlyTest:run?key="+os.getenv("GOOGLE_API_KEY"), data={"url": url})
-    if x.text.find("error") or x.text.find("PAGE_UNREACHABLE"):
+    if x.text.find("error")  > 0 or x.text.find("PAGE_UNREACHABLE") > 0:
         return -1
     if x.text.find("MOBILE_FRIENDLY"):
         return 1
